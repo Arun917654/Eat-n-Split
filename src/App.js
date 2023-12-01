245db367ed31ed284560c3121bd564bf96335bad
arun yadav <ay917654@gmail.com>
2023-12-01 19:26:59 +0530

    array update

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ import { FriendsList } from "./components/FriendsList";
 const initialFriends = [
   {
     id: 118836,
-    name: "Clark",
+    name: "Bhanu",
     image: "https://i.pravatar.cc/48?u=118836",
     balance: -7,
   },
   {
     id: 933372,
-    name: "Sarah",
+    name: "Ankit",
     image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
   },
   {
     id: 499476,
-    name: "Anthony",
+    name: "Aditya",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
